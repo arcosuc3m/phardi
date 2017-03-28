eda2103ebedae1f0861f59004bfabaf1fec1db68
Javier Garcia Blas <fjblas@inf.uc3m.es>
2017-03-28 17:43:51 +0200

    Added new copyright and license

--- include/create_kernel_for_qbi.hpp
+++ include/create_kernel_for_qbi.hpp
@@ -1,24 +1,21 @@
-/*
-Copyright (c) 2016 
-Javier Garcia Blas (fjblas@inf.uc3m.es)
-Jose Daniel Garcia Sanchez (josedaniel.garcia@uc3m.es)
-Yasser Aleman (yaleman@hggm.es)
-Erick Canales (ejcanalesr@gmail.com)
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
-associated documentation files (the "Software"), to deal in the Software without restriction, including 
-without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the 
-following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial 
-portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT 
-LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN 
-NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR 
-THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+/**
+* @version              pHARDI v0.3
+* @copyright            Copyright (C) 2017 Universidad Carlos III de Madrid. All rights reserved.
+* @license              GNU/GPL, see LICENSE.txt
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You have received a copy of the GNU General Public License in LICENSE.txt
+* also available in <http://www.gnu.org/licenses/gpl.html>.
+*
+* See COPYRIGHT.txt for copyright notices and details.
 */
 
 #ifndef CREATE_KERNEL_QBI_H
