adf39c6c297b3111369f29c02a25c5728ff9b5f4
Javier Garcia Blas <fjblas@inf.uc3m.es>
2017-07-02 20:56:23 +0200

    Updated superbuild

--- superbuild/OpenBLAS.cmake
+++ superbuild/OpenBLAS.cmake
@@ -1,12 +1,20 @@
-ExternalProject_Add(OpenBLAS
-  GIT_REPOSITORY "https://github.com/xianyi/OpenBLAS.git"
-  GIT_TAG "v0.2.19"
-  PREFIX deps/OpenBLAS
-  BUILD_IN_SOURCE
-  UPDATE_COMMAND ""
-  CMAKE_CACHE_ARGS
-    ## CXX should not be needed, but it a cmake default test
-    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
-    -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
-  )
+ExternalProject_add(OpenBLAS
+        GIT_REPOSITORY "git://github.com/xianyi/OpenBLAS.git"
+        GIT_TAG "v0.2.8"
+        UPDATE_COMMAND ""
+        BUILD_IN_SOURCE 1
+        PREFIX deps/OpenBLAS
+        CONFIGURE_COMMAND ""
+        PATCH_COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_LIST_DIR}/../external/Makefile.install.openblas" "<SOURCE_DIR>/Makefile.install"
+        BUILD_COMMAND NO_SHARED=1 ${CMAKE_MAKE_PROGRAM} libs netlib
+        INSTALL_COMMAND NO_SHARED=1 ${CMAKE_MAKE_PROGRAM} install PREFIX=<INSTALL_DIR>
+    )
+
+    set(Openblas_FOUND TRUE)
+    #set(Openblas_LIBRARIES ${Openblas_LIBRARY})
+    set(BLAS_LAPACK_LIBRARIES ${EXTERNAL_PREFIX}/lib/libopenblas.a)
+	if( CMAKE_COMPILER_IS_GNUCC)
+		list(APPEND BLAS_LAPACK_LIBRARIES gfortran pthread)
+	endif()
+    set(BLAS_LAPACK_FOUND ${Openblas_FOUND})
+    set(BLAS_LAPACK_INCLUDE_DIRS  ${EXTERNAL_PREFIX}/include)
